b0b51932f65bce22f14afa4d69f419054da34c14
Masha Rikhter <masha.rikhter@gmail.com>
2016-01-11 20:28:19 -0500

    Mv method to skip validation into public interface

--- example/app/models/membership.rb
+++ example/app/models/membership.rb
@@ -3,11 +3,11 @@ class Membership < ActiveRecord::Base
   belongs_to :person
   validates :person_id, uniqueness: { scope: :band_id, unless: :skip_uniqueness_validation? }
 
-  private
-
   def skip_uniqueness_validation!
     @skip_uniqueness_validation = true
   end
+
+  private
 
   def skip_uniqueness_validation?
     @skip_uniqueness_validation
