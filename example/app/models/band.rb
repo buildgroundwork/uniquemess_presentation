6ea2a88c9854a25cc44d67846891c92d75e9e5f3
Masha Rikhter <masha.rikhter@gmail.com>
2016-01-11 19:54:40 -0500

    Add our solution to rails app

--- example/app/models/band.rb
+++ example/app/models/band.rb
@@ -2,5 +2,21 @@ class Band < ActiveRecord::Base
   has_many :memberships
   accepts_nested_attributes_for :memberships,
     allow_destroy: true
+
+  before_validation :suppress_nested_uniqueness_validation_for_memberships
+  validate :unique_memberships
+
+  private
+
+  def suppress_nested_uniqueness_validation_for_memberships
+    memberships.each(&:skip_uniqueness_validation!)
+  end
+
+  def unique_memberships
+    person_ids = memberships.reject(&:marked_for_destruction?).collect(&:person_id)
+    if person_ids.size != person_ids.uniq.size
+      errors.add(:"memberships.person_id", I18n.translate('errors.messages.taken'))
+    end
+  end
 end
 
--- example/app/models/membership.rb
+++ example/app/models/membership.rb
@@ -1,6 +1,16 @@
 class Membership < ActiveRecord::Base
   belongs_to :band
   belongs_to :person
-  validates :person_id, uniqueness: { scope: :band_id }
+  validates :person_id, uniqueness: { scope: :band_id, unless: :skip_uniqueness_validation? }
+
+  private
+
+  def skip_uniqueness_validation!
+    @skip_uniqueness_validation = true
+  end
+
+  def skip_uniqueness_validation?
+    @skip_uniqueness_validation
+  end
 end
 
